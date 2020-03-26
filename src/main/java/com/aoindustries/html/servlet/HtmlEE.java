ec26890ed9d99e0dd721182da78bbef0c7a90628
AO Industries, Inc. <support@aoindustries.com>
2020-03-26 07:21:43 -0500

    New methods MediaWriter.text(…) for writing arbitrary textual data.
    Automatically determines correct encoding.  Supports encoder prefixes
    and suffixes as well as in-context translation.

--- src/main/java/com/aoindustries/html/servlet/HtmlEE.java
+++ src/main/java/com/aoindustries/html/servlet/HtmlEE.java
@@ -22,13 +22,19 @@
  */
 package com.aoindustries.html.servlet;
 
-import com.aoindustries.encoding.ChainWriter;
+import com.aoindustries.encoding.Doctype;
+import com.aoindustries.encoding.Serialization;
 import com.aoindustries.encoding.servlet.DoctypeEE;
+import com.aoindustries.encoding.servlet.EncodingContextEE;
 import com.aoindustries.encoding.servlet.SerializationEE;
 import com.aoindustries.html.Html;
+import java.io.IOException;
 import java.io.Writer;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Fluent Java DSL for high-performance HTML generation in a Servlet environment.
@@ -40,31 +46,45 @@ final public class HtmlEE {
 	// Make no instances
 	private HtmlEE() {}
 
-	public static Html get(ServletContext servletContext, HttpServletRequest request, Writer out) {
+	public static Html get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, Writer out) {
+		Serialization serialization = SerializationEE.get(servletContext, request);
+		Doctype doctype = DoctypeEE.get(servletContext, request);
 		return new Html(
-			SerializationEE.get(servletContext, request),
-			DoctypeEE.get(servletContext, request),
+			// TODO: Should we continue to override this context to be fixed values, as we are doing here?
+			// TODO: Seems that context changing with Html not changing would be reasonable?
+			// TODO: Or don't store serialization at all?  This, however, would incur more lookups and what would happen if changing output mid-page?
+			new EncodingContextEE(servletContext, request, response) {
+				@Override
+				public Serialization getSerialization() {
+					return serialization;
+				}
+				@Override
+				public Doctype getDoctype() {
+					return doctype;
+				}
+			},
+			serialization,
+			doctype,
 			out
 		);
 	}
 
-	public static Html get(HttpServletRequest request, Writer out) {
-		return get(request.getServletContext(), request, out);
+	/**
+	 * @see  ServletResponse#getWriter()
+	 */
+	public static Html get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		return get(servletContext, request, response, response.getWriter());
+	}
+
+	public static Html get(HttpServletRequest request, HttpServletResponse response, Writer out) {
+		return get(request.getServletContext(), request, response, out);
 	}
 
 	/**
-	 * Unwraps the given chain writer.
+	 * @see  ServletRequest#getServletContext()
+	 * @see  ServletResponse#getWriter()
 	 */
-	// TODO: Get rid of these, and the dependency on ao-encoding, once we're no longer so ChainWriter-focused
-	public static Html get(ServletContext servletContext, HttpServletRequest request, ChainWriter out) {
-		return get(servletContext, request, out.getPrintWriter());
-	}
-
-	/**
-	 * Unwraps the given chain writer.
-	 */
-	// TODO: Get rid of these, and the dependency on ao-encoding, once we're no longer so ChainWriter-focused
-	public static Html get(HttpServletRequest request, ChainWriter out) {
-		return get(request.getServletContext(), request, out.getPrintWriter());
+	public static Html get(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		return get(request.getServletContext(), request, response, response.getWriter());
 	}
 }
