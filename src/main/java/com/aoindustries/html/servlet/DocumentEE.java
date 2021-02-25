ef38a5eed51d5e7ad474d0693ff7eab99046c6f2
AO Industries, Inc <support@aoindustries.com>
2021-02-24 23:33:44 -0600

    Implemented tab indentation.

--- src/main/java/com/aoindustries/html/servlet/DocumentEE.java
+++ src/main/java/com/aoindustries/html/servlet/DocumentEE.java
@@ -42,20 +42,132 @@ final public class DocumentEE {
 	// Make no instances
 	private DocumentEE() {}
 
-	public static Document get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, Writer out) {
+	/**
+	 * Context init parameter that may be used to configure the default document indent within an application.
+	 * Must be one of "true", "false", or "auto" (the default).
+	 */
+	public static final String INDENT_INIT_PARAM = Document.class.getName() + ".indent";
+
+	/**
+	 * Determines the default document indent by first checking for {@linkplain ServletContext#getInitParameter(java.lang.String) context-param}
+	 * of {@link #INDENT_INIT_PARAM}, then using {@code false} when unspecified or "auto".
+	 */
+	public static boolean getDefaultIndent(ServletContext servletContext) {
+		String initParam = servletContext.getInitParameter(INDENT_INIT_PARAM);
+		if(initParam != null) {
+			initParam = initParam.trim();
+			if(!initParam.isEmpty()) {
+				if("true".equalsIgnoreCase(initParam)) {
+					return true;
+				} else if("false".equalsIgnoreCase(initParam)) {
+					return false;
+				} else if(!"auto".equalsIgnoreCase(initParam)) {
+					throw new IllegalArgumentException("Unexpected value for " + INDENT_INIT_PARAM + ": Must be one of \"true\", \"false\", or \"auto\": " + initParam);
+				}
+			}
+		}
+		return false;
+	}
+
+	private static final String INDENT_REQUEST_ATTRIBUTE = INDENT_INIT_PARAM;
+
+	/**
+	 * Registers the document indent in effect for the request.
+	 * <p>
+	 * This does not change existing instances of {@link Document};
+	 * it only affects the configuration of new instances.
+	 * </p>
+	 */
+	public static void setIndent(ServletRequest request, Boolean indent) {
+		request.setAttribute(INDENT_REQUEST_ATTRIBUTE, indent);
+	}
+
+	/**
+	 * Replaces the document indent in effect for the request.
+	 * <p>
+	 * This does not change existing instances of {@link Document};
+	 * it only affects the configuration of new instances.
+	 * </p>
+	 *
+	 * @return  The previous attribute value, if any
+	 */
+	public static Boolean replaceIndent(ServletRequest request, Boolean indent) {
+		Boolean old = (Boolean)request.getAttribute(INDENT_REQUEST_ATTRIBUTE);
+		request.setAttribute(INDENT_REQUEST_ATTRIBUTE, indent);
+		return old;
+	}
+
+	/**
+	 * Gets the document indent in effect for the request, or {@linkplain #getDefaultIndent(javax.servlet.ServletContext) the default}
+	 * when not yet {@linkplain #setIndent(javax.servlet.ServletRequest, java.lang.Boolean) set}.
+	 * <p>
+	 * Once the default is resolved,
+	 * {@linkplain #setIndent(javax.servlet.ServletRequest, java.lang.Boolean) sets the request attribute}.
+	 * </p>
+	 * <p>
+	 * This does not change existing instances of {@link Document};
+	 * it only affects the configuration of new instances.
+	 * </p>
+	 */
+	public static boolean getIndent(ServletContext servletContext, ServletRequest request) {
+		Boolean indent = (Boolean)request.getAttribute(INDENT_REQUEST_ATTRIBUTE);
+		if(indent == null) {
+			indent = getDefaultIndent(servletContext);
+			request.setAttribute(INDENT_REQUEST_ATTRIBUTE, indent);
+		}
+		return indent;
+	}
+
+	public static Document get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, Writer out, boolean indent) {
 		return new Document(
 			new EncodingContextEE(servletContext, request, response),
 			out
+		).setIndent(indent);
+	}
+
+	/**
+	 * Uses the {@linkplain #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest) current or default indentation settings}.
+	 *
+	 * @see  #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest)
+	 * @see  #get(javax.servlet.ServletContext, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.io.Writer, boolean)
+	 */
+	public static Document get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, Writer out) {
+		return get(
+			servletContext, request, response, out,
+			getIndent(servletContext, request)
 		);
 	}
 
 	/**
 	 * @see  ServletResponse#getWriter()
 	 */
-	public static Document get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response) throws IOException {
-		return get(servletContext, request, response, response.getWriter());
+	public static Document get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, boolean indent) throws IOException {
+		return get(servletContext, request, response, response.getWriter(), indent);
 	}
 
+	/**
+	 * Uses the {@linkplain #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest) current or default indentation settings}.
+	 *
+	 * @see  #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest)
+	 * @see  #get(javax.servlet.ServletContext, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, boolean)
+	 */
+	public static Document get(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		return get(
+			servletContext, request, response,
+			getIndent(servletContext, request)
+		);
+	}
+
+	public static Document get(HttpServletRequest request, HttpServletResponse response, Writer out, boolean indent) {
+		return get(request.getServletContext(), request, response, out, indent);
+	}
+
+	/**
+	 * Uses the {@linkplain #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest) current or default indentation settings}.
+	 *
+	 * @see  #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest)
+	 * @see  #get(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.io.Writer, boolean)
+	 */
 	public static Document get(HttpServletRequest request, HttpServletResponse response, Writer out) {
 		return get(request.getServletContext(), request, response, out);
 	}
@@ -63,6 +175,16 @@ final public class DocumentEE {
 	/**
 	 * @see  ServletRequest#getServletContext()
 	 * @see  ServletResponse#getWriter()
+	 */
+	public static Document get(HttpServletRequest request, HttpServletResponse response, boolean indent) throws IOException {
+		return get(request.getServletContext(), request, response, response.getWriter(), indent);
+	}
+
+	/**
+	 * Uses the {@linkplain #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest) current or default indentation settings}.
+	 *
+	 * @see  #getIndent(javax.servlet.ServletContext, javax.servlet.ServletRequest)
+	 * @see  #get(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, boolean)
 	 */
 	public static Document get(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		return get(request.getServletContext(), request, response, response.getWriter());
