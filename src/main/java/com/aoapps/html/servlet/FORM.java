49f38960de0dcc358a861b291ee97984e3c81eef
AO Industries, Inc <support@aoindustries.com>
2022-03-03 09:55:16 -0600

    TODO: Write name attribute also when id is set?

--- src/main/java/com/aoapps/html/servlet/FORM.java
+++ src/main/java/com/aoapps/html/servlet/FORM.java
@@ -1,6 +1,6 @@
 /*
  * ao-fluent-html-servlet - Fluent Java DSL for high-performance HTML generation in a Servlet environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -36,6 +36,7 @@ import java.io.Writer;
  *
  * @author  AO Industries, Inc.
  */
+// TODO: Write name attribute also when id is set?  At least by doctype?  See ao-taglib
 public final class FORM<
 	PC extends PalpableContent<PC>
 > extends
